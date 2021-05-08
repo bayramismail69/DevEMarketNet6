b7b3f6b44a129461d7f437d7d0b68710597ab192
BAYRAM <bayramismail69@gmail.com>
2021-05-08 16:31:45 +0300

    elastic search added

--- Core/Utilities/Business/BusinessRules.cs
+++ Core/Utilities/Business/BusinessRules.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Utilities.Results;
 
 namespace Core.Utilities.Business
 {
     public class BusinessRules
     {
+        public static IResult Run(params IResult[] logics)
+        {
+            foreach (var result in logics)
+            {
+                if (!result.Success)
+                {
+                    return result;
+                }
+            }
+
+            return null;
+        }
     }
 }
