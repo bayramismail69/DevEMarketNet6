153009708c84a9b67190667ca0c34e34aac20bae
BAYRAM <bayramismail69@gmail.com>
2021-05-08 20:32:14 +0300

    Elastic Search updated

--- Core/Utilities/ElasticSearch/Models/IndexModel.cs
+++ Core/Utilities/ElasticSearch/Models/IndexModel.cs
@@ -10,6 +10,8 @@ namespace Core.Utilities.ElasticSearch.Models
     {
         public string IndexName { get; set; }
         public string AliasName { get; set; }
-        public int NumberOfReplicas { get; set; }
+        public int NumberOfReplicas { get; set; } = 3;
+        public int NumberOfShards { get; set; } = 3;
     }
 }
+ 
