ae5d9dc3a6ac413d70d804e45bb939c449b6409e
BAYRAM <bayramismail69@gmail.com>
2021-05-08 15:17:33 +0300

    Add project files.

---
+++ Core/Entities/DocumentDbEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace Core.Entities
+{
+    public class DocumentDbEntity
+    {
+        public ObjectId Id { get; set; }
+    }
+}
