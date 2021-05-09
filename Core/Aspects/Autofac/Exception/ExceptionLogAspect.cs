cd33322d4e2f53880c8330a4f5be77c57d7b9831
BAYRAM <bayramismail69@gmail.com>
2021-05-09 03:25:25 +0300

    Elastich searc updated and logger added

---
+++ Core/Aspects/Autofac/Exception/ExceptionLogAspect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.CrossCuttingConcerns.Logging.Serilog;
+using Microsoft.AspNetCore.Http;
+using Core.Utilities.Interceptors;
+
+namespace Core.Aspects.Autofac.Exception
+{
+   public class ExceptionLogAspect:MethodInterception
+   {
+       private readonly LoggerServiceBase _loggerServiceBase;
+       private readonly IHttpContextAccessor _httpContextAccessor;
+
+       public ExceptionLogAspect(Type loggerService)
+       {
+           if (loggerService.BaseType!=typeof(LoggerServiceBase))
+           {
+               throw new ArgumentException();
+               //burda kaldım
+           }
+       }
+   }
+}
---
+++ Core/CrossCuttingConcerns/Logging/Serilog/Loggers/LogstashLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Core.CrossCuttingConcerns.Logging.Serilog.ConfigurationModels;
+using Core.Utilities.IoC;
+using Core.Utilities.Messages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Formatting.Elasticsearch;
+using Serilog.Sinks.Http.BatchFormatters;
+
+namespace Core.CrossCuttingConcerns.Logging.Serilog.Loggers
+{
+   public class LogstashLogger:LoggerServiceBase
+    {
+        public LogstashLogger()
+        {
+            IConfiguration configuration = ServiceTool.ServiceProvider.GetService<IConfiguration>();
+            var logConfig = configuration.GetSection("SeriLogConfigurations:LogstashConfiguration")
+                                .Get<LogstashConfiguration>() ??
+                            throw new Exception(SerilogMessages.NullOptionsMessage);
+            var seriLogConfig=new LoggerConfiguration().WriteTo
+                .DurableHttpUsingFileSizeRolledBuffers(
+                    requestUri: logConfig.URL,
+                    batchFormatter: new ArrayBatchFormatter(),
+                    textFormatter: new ElasticsearchJsonFormatter()
+                    )
+                .CreateLogger();
+            _logger = seriLogConfig;
+        }
+    }
+}
